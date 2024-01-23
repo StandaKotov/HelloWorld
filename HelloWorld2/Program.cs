ecdd0ffea72e0e8d2703e4ee3eda8ad766c2a40b
StandaKotov <dp201090ksj@gmail.com>
2024-01-22 21:19:56 -0600

    выводит текст или значения на консоль с добавлением перевода строки

---
+++ HelloWorld2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace HelloWorld2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Выводим на экрант текст
+            // Потому что никак иначе
+            Console.WriteLine("Hello, Stas!");
+            //Console.WriteLine("Hello, World!");
+            Console.WriteLine("Hello, Sergey!");
+        }
+    }
+}
