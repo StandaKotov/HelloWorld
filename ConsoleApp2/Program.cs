9af98aa2c9bf2cab7f042f7f6e10b48eaf066290
StandaKotov <dp201090ksj@gmail.com>
2024-01-28 19:51:10 -0600

    Домашка 5

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Text;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = Encoding.UTF8;
+
+            Console.WriteLine("введите ваше имя");
+
+            string name = Console.ReadLine();
+
+      
+
+            Console.WriteLine("Введите вашу фамилию");
+            
+            string suname = Console.ReadLine();
+
+            
+            Console.WriteLine("Введите ваш возраст");
+
+            string age = Console.ReadLine();
+
+            int conevrtedAge = int.Parse(age);
+
+            Console.WriteLine("Введите вашу профессию");
+            string profession = Console.ReadLine();
+
+
+            Console.WriteLine("Привет, " + name+ " " + suname +" " + "я не думал что вам" + " " + age + "." + profession +" " + "-"+ " " + "это очень интересная профессия" );
+            
+        }
+    }
+}
